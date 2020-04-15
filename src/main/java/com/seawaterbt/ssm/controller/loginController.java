824d8703b8a9d7f74c63251b6e0ef96c5c870052
zsx <1070676593@qq.com>
2020-04-15 17:55:17 +0800

    创建分支2后1再次提交

--- src/main/java/com/seawaterbt/ssm/controller/loginController.java
+++ src/main/java/com/seawaterbt/ssm/controller/loginController.java
@@ -11,5 +11,6 @@ public class loginController {
         System.out.println("分支已经合并。。。。。");
         System.out.println("zsx1");
         System.out.println("zsx1再次提交");
+        System.out.println("创建分支zsx2后再次提交");
     }
 }
