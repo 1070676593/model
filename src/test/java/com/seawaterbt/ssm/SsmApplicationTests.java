ffba8f0f136b2f0b67bcf029233c9e5d4ade1c9f
zsx <1070676593@qq.com>
2021-04-19 10:07:56 +0800

    主分支提交，不提交到git上面

--- src/test/java/com/seawaterbt/ssm/SsmApplicationTests.java
+++ src/test/java/com/seawaterbt/ssm/SsmApplicationTests.java
@@ -13,6 +13,7 @@ public class SsmApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println("主分支");
+        System.out.println("zsx-fenzhi1提交，但不提交到git上");
     }
 
 }
